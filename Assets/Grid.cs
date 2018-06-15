7a35e78c27240946ae23a394aec1ec9419d02e0a
DESKTOP-IKDVREU\박수환 <sksk31052@hanmail.net>
2018-06-15 20:51:58 +0900

    Cost calculating
    cost calculating added

--- Assets/Grid.cs
+++ Assets/Grid.cs
@@ -6,6 +6,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Grid : MonoBehaviour
 {
@@ -16,11 +17,29 @@ public class Grid : MonoBehaviour
     private int numOfGrid;
     [SerializeField] private bool[] accessible;
 
+    public Text gridText;
+
+    int departPoint;
+    int destPoint;
+
+    [SerializeField] private List<int> openList;
+    [SerializeField] private List<int> closedList;
+
+    private int[] gCost;
+    private int[] hCost;
+    private int[] fCost;
+
     // Use this for initialization
     void Start()
     {
         // Setting up initial field options
         numOfGrid = sizeOfField / gridSize;
+
+        departPoint = -1;
+        destPoint = -1;
+
+        openList = new List<int>();
+        closedList = new List<int>();
 
 
         // Initialize transform values
@@ -35,9 +54,18 @@ public class Grid : MonoBehaviour
             Debug.DrawLine(new Vector3(0, 0, a * gridSize), new Vector3(sizeOfField, 0, a * gridSize), Color.green, Mathf.Infinity);
         }
 
+        gCost = new int[numOfGrid * numOfGrid];
+        hCost = new int[numOfGrid * numOfGrid];
+        fCost = new int[numOfGrid * numOfGrid];
+
         accessible = new bool[numOfGrid * numOfGrid];
+
         for (int a = 0; a < numOfGrid * numOfGrid; a++)
         {
+            gCost[a] = -1;
+            hCost[a] = -1;
+            fCost[a] = -1;
+
             if (Grid2Vector(a).y > .5f) accessible[a] = false;
             else accessible[a] = true;
         }
@@ -47,24 +75,42 @@ public class Grid : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(1))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit, Mathf.Infinity))
             {
-                //print(Vector2Grid(hit.point));
-                //print(GridDistance(Vector2Grid(selectedObj.transform.position), Vector2Grid(hit.point)));
-                GetNeighbor(Vector2Grid(hit.point));
+                departPoint = Vector2Grid(selectedObj.transform.position);
+                destPoint = Vector2Grid(hit.point);
+
+                Debug.DrawLine(new Vector3((int)hit.point.x - .5f, .5f, (int)hit.point.z - .5f), new Vector3((int)hit.point.x + .5f, .5f, (int)hit.point.z + .5f), Color.red, Mathf.Infinity);
+                FindPath(departPoint, destPoint);
+
+                //GetNeighbor(Vector2Grid(hit.point));
             }
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                gridText.text = Vector2Grid(hit.point).ToString();
+                print("GridNum : " + Vector2Grid(hit.point));
+                print("Gcost : " + gCost[Vector2Grid(hit.point)]);
+                print("Hcost : " + hCost[Vector2Grid(hit.point)]);
+                print("Fcost : " + fCost[Vector2Grid(hit.point)]);
+            }
+
         }
     }
 
     List<int> GetNeighbor(int gridNum)
     {
         List<int> neighbors = new List<int>() { -1, 1, -numOfGrid, numOfGrid, -numOfGrid - 1, -numOfGrid + 1, numOfGrid - 1, numOfGrid + 1 };
-        string str = "";
+        //string str = "";
 
         if (gridNum % numOfGrid == 0) neighbors.RemoveAll((num) => { return num == -1 || num == -1 - numOfGrid || num == -1 + numOfGrid; });
         if (gridNum % numOfGrid == numOfGrid - 1) neighbors.RemoveAll((num) => { return num == 1 || num == 1 - numOfGrid || num == 1 + numOfGrid; });
@@ -74,9 +120,9 @@ public class Grid : MonoBehaviour
         for (int a = 0; a < neighbors.Count; a++)
         {
             neighbors[a] += gridNum;
-            str = str + neighbors[a].ToString() + ", ";
+            //str = str + neighbors[a].ToString() + ", ";
         }
-        print(str);
+        //print(str);
         return neighbors;
     }
 
@@ -85,10 +131,18 @@ public class Grid : MonoBehaviour
     {
         // Finding gap between two grids
         int gridGap = Mathf.Abs(gridA - gridB);
-
+        print(gridA + ", " + gridB + " Gap : " + gridGap);
         // To find diagonal distance, Got to be compared which row or column is bigger
-        if (gridGap / numOfGrid > gridGap % numOfGrid) return (gridGap / numOfGrid * 10) + (gridGap % numOfGrid * 4);
-        else return (gridGap / numOfGrid * 4) + (gridGap % numOfGrid * 10);
+        if (gridGap > numOfGrid)
+        {
+            if (numOfGrid / gridGap > gridGap % numOfGrid) return (gridGap / numOfGrid * 10) + (gridGap % numOfGrid * 4);
+            else return (gridGap / numOfGrid * 4) + (gridGap % numOfGrid * 10);
+        }
+        else
+        {
+            if (gridGap / numOfGrid > gridGap % numOfGrid) return (gridGap / numOfGrid * 10) + (gridGap % numOfGrid * 4);
+            else return (gridGap / numOfGrid * 4) + (gridGap % numOfGrid * 10);
+        }
     }
 
     // Convert Vector3 position to Grid number
@@ -102,4 +156,47 @@ public class Grid : MonoBehaviour
     {
         return new Vector3((grid % (numOfGrid)) * gridSize + gridSize / 2, .5f, (grid / (numOfGrid) * gridSize + gridSize / 2));
     }
+
+    void FindCost(int gridNum)
+    {
+        gCost[gridNum] = GridDistance(departPoint, gridNum);
+        hCost[gridNum] = GridDistance(gridNum, destPoint);
+        fCost[gridNum] = gCost[gridNum] + hCost[gridNum];
+    }
+
+    void FindPath(int depart, int dest)
+    {
+        int curGrid = -1;
+        
+        openList.AddRange(GetNeighbor(depart));
+        foreach (int nList in openList)
+        {
+            FindCost(nList);
+        }
+        //for (int a = 0; a < 1; a++)
+        //{
+        //    foreach (int nList in openList)
+        //    {
+        //        if (fCost[nList] != -1) continue;
+        //        openList.Add(nList);
+        //    }
+        //}
+
+        foreach (int gList in openList)
+        {
+            print(gList);
+        }
+
+        //while(curGrid != dest)
+        //{
+        //    int minOpen = openList[0];
+        //    foreach(int oList in openList)
+        //    {
+        //        if (minOpen > oList) minOpen = oList;
+        //    }
+
+
+        //}
+    }
+
 }
